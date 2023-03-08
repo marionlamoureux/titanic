efcbca5fe8dd3bb6349c818cac1bba9dd80e07a5
Marion Lamoureux <mlamoureux@cloudera.com>
2023-03-08 14:24:37 +0000

    model logistic regression

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # titanic
+This is inspired by Kaggle challenge Titanic
+https://www.kaggle.com/competitions/titanic
+
+Data are available here :
+https://www.kaggle.com/competitions/titanic/data
+
+For data exploration, this workbook is extremely clear:
+https://www.kaggle.com/code/seuwenfei/titanic-random-forest-cv-score-0-85
+
+
 Step 1 - Push the raw data in a HIVE table
 Code snippets are available, pointing to existing data lakes
 ![](images/CodeSnippet_HIVEConnection.png)
@@ -7,4 +17,5 @@ Code snippets are available, pointing to existing data lakes
 Step 2 -  Writing in python the function for the model
 Tutorials for building ML models are here :
 https://www.cloudera.com/tutorials.html
+
 
--- code.py
+++ code.py
@@ -45,10 +45,52 @@ titanic_data = spark.read.csv(path, header=True, sep=",", nullValue="NA")
 # ...and push csv into hive table
 titanic_data.write.saveAsTable("titanic.titanic_train")
 
+# Step 2 - Build the model
 # On the training data, create a logistic regression model
 # Model simply needs to be a function so we can deploy it.
 
-import 
+!pip install scikit-learn
+!pip install pandas
+!pip install numpy
+
+import sys
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+from sklearn.preprocessing import LabelEncoder
+import pickle
+
+# decode input(s)
+
+df = pd.read_csv('titanic/raw data/train.csv', sep=',',header=0).dropna(subset=['Pclass', 'Sex', 'Age', 'SibSp', 'Parch','Fare', 'Embarked', 'Survived'])
+
+# extract data and encode it (using label encoder)
+# We're dropping PassengerID, Name, Ticket number, Cabin that are clearly irrelevant
+# and label encoding the none numeric features.
+
+df['Encoded_Sex'] = LabelEncoder().fit_transform(df.Sex)
+df['Encoded_Embarked'] = LabelEncoder().fit_transform(df.Embarked)
+
+features = df[[ 'Pclass', 'Encoded_Sex', 'Age', 'SibSp', 'Parch','Fare', 'Encoded_Embarked']]
+survived = df[['Survived']]
+
+regModel = LinearRegression()
+regModel.fit(features,survived)
+
+
+# Validate Model
+scores = cross_val_score(regModel, features, survived, cv=3)
+avgAcc = np.mean(scores)
+print(avgAcc)
+
+# Save Model
+mdl = pickle.dumps(regModel)
+with open('titanic_LogRegression.pickle', 'wb') as handle:
+	pickle.dump(mdl, handle)
+  
+
 
 
 
